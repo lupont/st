51bcba2805b8ef4cc1e470c3a5d6e7b189cabf93
Pontus Laos <laos.pontus@gmail.com>
2022-03-13 12:41:04 +0100

    patch: gruvbox

--- config.h
+++ config.h
@@ -96,32 +96,50 @@ unsigned int tabspaces = 4;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
+	// "black",
+	// "red3",
+	// "green3",
+	// "yellow3",
+	// "blue2",
+	// "magenta3",
+	// "cyan3",
+	// "gray90",
+
+ 	[0] = "#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
+ 	[1] = "#cc241d", /* red     */
+ 	[2] = "#98971a", /* green   */
+ 	[3] = "#d79921", /* yellow  */
+ 	[4] = "#458588", /* blue    */
+ 	[5] = "#b16286", /* magenta */
+ 	[6] = "#689d6a", /* cyan    */
+ 	[7] = "#a89984", /* white   */
+ 	[8]  = "#928374", /* black   */
+ 	[9]  = "#fb4934", /* red     */
+ 	[10] = "#b8bb26", /* green   */
+ 	[11] = "#fabd2f", /* yellow  */
+ 	[12] = "#83a598", /* blue    */
+ 	[13] = "#d3869b", /* magenta */
+ 	[14] = "#8ec07c", /* cyan    */
+ 	[15] = "#ebdbb2", /* white   */
+    [16] = "#090909",
 
 	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+	// "gray50",
+	// "red",
+	// "green",
+	// "yellow",
+	// "#5c5cff",
+	// "magenta",
+	// "cyan",
+	// "white",
 
-	[255] = 0,
+	// [255] = 0,
 
-	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
-	"gray90", /* default foreground colour */
-	"#090909", /* default background colour */
+	// /* more colors can be added after 255 to use with DefaultXX */
+	// "#cccccc",
+	// "#555555",
+	// "gray90", /* default foreground colour */
+	// "#090909", /* default background colour */
 };
 
 
@@ -129,9 +147,9 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 258;
-unsigned int defaultbg = 259;
-unsigned int defaultcs = 256;
+unsigned int defaultfg = 15;
+unsigned int defaultbg = 16;
+unsigned int defaultcs = 15;
 static unsigned int defaultrcs = 257;
 
 /*
